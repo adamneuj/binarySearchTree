cde475d0cd596d7f9299f2dde1efc948f8f01fdf
Adam Neujahr <adamneujahr@gmail.com>
2019-10-14 14:15:11 -0500

    Started add function.

---
+++ binarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binarySearchTree
+{
+    class BinarySearchTree
+    {
+        Node start;
+        public BinarySearchTree()
+        {
+            
+        }
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if(start == null)
+            {
+                start = node;
+                return;
+            }
+
+        }
+    }
+}
---
+++ binarySearchTree/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binarySearchTree
+{
+    class Node
+    {
+        public int data;
+        public Node root;
+        public Node rightChild;
+        public Node leftChild;
+
+        public Node(int value)
+        {
+            data = value;
+        }
+    }
+}
